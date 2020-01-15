1804ae3e80ecad9315af5ef46826f7f2c414b4af
Debasis Mohapatra <ds.tuation@gmail.com>
2020-01-15 10:16:29 +0530

    added main and test class

---
+++ src/main/java/com/tuation/common/service/CommonService.java
@@ -0,0 +1,12 @@
+package com.tuation.common.service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonService.class, args);
+	}
+
+}
